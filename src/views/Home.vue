c51024fe6643e9c9bae2328f2923722bbf36bade
YC <1011912556@qq.com>
2020-03-24 20:50:57 +0800

    first

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <van-button type="default">默认按钮</van-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home'
+}
+</script>
