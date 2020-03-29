289c46d709a38497e28372d52cb8b81705986e47
大元 <596654003@qq.com>
2020-03-29 12:07:04 +0800

    update tabbar

--- src/router/index.js
+++ src/router/index.js
@@ -6,39 +6,118 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
     path: '/login',
     name: 'login',
     component: () => import('../views/Login.vue')
   },
   {
-    path: '/regester',
-    name: 'regester',
-    component: () => import('../views/Regester.vue')
+    path: '/',
+    name: 'Home',
+    component: Home
   },
   {
-    path: '/oldcarmsg',
-    component: () => import('../views/OldcarMsg.vue')
+    path: '',
+    component: Home,
+    children: [
+      {
+        path: '/homeIndex',
+        component: () => import('../views/home/index.vue'),
+        meta: {
+          navShow: true,
+          title: '首页'
+        }
+      },
+      {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+        meta: {
+          navShow: true,
+          title: '关于'
+        }
+      },
+      {
+        path: '/oldcarmsg',
+        component: () => import('../views/OldcarMsg.vue'),
+        meta: {
+          navShow: true,
+          title: '二手车'
+        }
+      },
+      {
+        path: '/carlist',
+        component: () => import('../views/carList.vue'),
+        meta: {
+          navShow: true,
+          title: '车列表'
+        }
+      },
+      {
+        path: '/yysq',
+        component: () => import('../views/Yysq.vue'),
+        meta: {
+          navShow: true,
+          title: ''
+        }
+      },
+      {
+        path: '/regester',
+        name: 'regester',
+        component: () => import('../views/Regester.vue'),
+        meta: {
+          navShow: true,
+          title: ''
+        }
+      },
+    ]
   },
   {
-    path: '/carlist',
-    component: () => import('../views/carList.vue')
-  },
-  {
-    path: '/yysq',
-    component: () => import('../views/Yysq.vue')
+    path: '',
+    component: Home,
+    children: [
+      {
+        path: '/serviceManagent',
+        component: () => import('../views/serviceManagent/index'),
+        meta: {
+          navShow: true,
+          title: '服务管理'
+        }
+      },
+      {
+        path: '/serviceManagent/insurance',
+        component: () => import('../views/serviceManagent/insurance'),
+        meta: {
+          navShow: true,
+          title: '保险模块'
+        }
+      },
+      {
+        path: '/serviceManagent/serve',
+        component: () => import('../views/serviceManagent/serve'),
+        meta: {
+          navShow: true,
+          title: '服务模块'
+        }
+      }, {
+        path: '/serviceManagent/inform',
+        component: () => import('../views/serviceManagent/inform'),
+        meta: {
+          navShow: true,
+          title: '通知管理'
+        }
+      }, {
+        path: '/serviceManagent/bargain',
+        component: () => import('../views/serviceManagent/bargain'),
+        meta: {
+          navShow: true,
+          title: '砍价模块'
+        }
+      }
+
+    ]
   }
 ]
 
