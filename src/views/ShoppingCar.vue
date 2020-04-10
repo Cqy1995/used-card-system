fd5c66665ea604bb4f24fafe6658c694bc657f7c
Yc <1011912556@qq.com>
2020-04-10 18:10:40 +0800

    今天改的

--- src/views/ShoppingCar.vue
+++ src/views/ShoppingCar.vue
@@ -2,25 +2,16 @@
   <div class="shoppingcar">
     <van-nav-bar title="购物车"  @click-left="onClickLeft" left-arrow/>
     <van-card
-        price="6.79万"
-        desc="2016款 蓝标 1.5T自动两驱精英型"
-        title="哈弗H6"
-        thumb="http://e.hiphotos.baidu.com/image/h%3D300/sign=cd92f6ddf903738dc14a0a22831ab073/08f790529822720e78606b8774cb0a46f21fab25.jpg"
+    v-for="(item,index) in carList"
+        :price="item.price"
+        :desc="item.desc"
+        :title="item.title"
+        :thumb="item.thumb"
       >
-      </van-card>
-      <van-card
-        price="6.79万"
-        desc="2017款 蓝标 1.5T自动舒适型"
-        title="力帆轩朗"
-        thumb="http://img4.imgtn.bdimg.com/it/u=3331455421,259079271&fm=26&gp=0.jpg"
-      >
-      </van-card>
-      <van-card
-        price="6.79万"
-        desc="2013款 蓝标 1.8TSI标配型"
-        title="大众夏朗"
-        thumb="http://img4.imgtn.bdimg.com/it/u=2388327870,2514168806&fm=26&gp=0.jpg"
-      >
+      <template #footer>
+    <van-button size="mini" @click="Toedit(index)">修改</van-button>
+    <van-button size="mini" @click="Todel(index)">删除</van-button>
+  </template>
       </van-card>
       <van-submit-bar
   :price="19000000"
@@ -35,7 +26,30 @@ export default {
   data() {
     return {
       active: 0,
-      value: ""
+      value: "",
+      carList: [
+        {
+          price: '6.79万',
+          desc: '2013款 蓝标 1.8TSI标配型',
+          title: '大众夏朗',
+          thumb: 'http://img4.imgtn.bdimg.com/it/u=2388327870,2514168806&fm=26&gp=0.jpg'
+          
+        },
+        {
+          price: '6.79万',
+          desc: '2017款 蓝标 1.5T自动舒适型',
+          title: '力帆轩朗',
+          thumb: 'http://img4.imgtn.bdimg.com/it/u=3331455421,259079271&fm=26&gp=0.jpg'
+          
+        },
+        {
+          price: '6.79万',
+          desc: '2016款 蓝标 1.5T自动两驱精英型',
+          title: '哈弗H6',
+          thumb: 'http://e.hiphotos.baidu.com/image/h%3D300/sign=cd92f6ddf903738dc14a0a22831ab073/08f790529822720e78606b8774cb0a46f21fab25.jpg'
+          
+        }
+      ]
     };
   },
   methods: {
@@ -44,6 +58,12 @@ export default {
     },
     showPopup() {
       this.$router.push("/serviceManagent/insuranceDetail");
+    },
+    Toedit(a) {
+      this.$toast("改变颜色")
+    },
+    Todel(a) {
+      this.carList.splice(a,1);
     },
     onSubmit() {
       this.$router.push("/user")
