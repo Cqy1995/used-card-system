732a33b0703b74b466ab1a16e9bc3db18f94d850
qiyuan chen <44863037+Cqy1995@users.noreply.github.com>
2021-02-05 11:46:45 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,13 +10,13 @@ const tousu = require('./tousu/tousuapi'); //投诉
 const kanjia = require('./kanjia/kanjiaapi'); //砍价
 const orderlist = require('./orderList/orderlistapi'); //提交订单
 const consoult = require('./consoulemsg/consoulemsgapi')
-    // 引入文件模块
+// 引入文件模块
 const fs = require('fs');
 // 引入处理路径的模块
 const path = require('path');
-// 引入处理post数据的模块
+// 引入处理post数据的模块，原生需要使用on,end监听到post请求的内容，这属于三方中间件
 const bodyParser = require('body-parser')
-    // 引入Express
+// 引入Express
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -31,13 +31,14 @@ app.all('*', function(req, res, next) {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
         next();
     })
-    // 访问静态资源文件 这里是访问所有dist目录下的静态资源文件
+// 访问静态资源文件 这里是访问所有dist目录下的静态资源文件
 app.use(express.static(path.resolve(__dirname, '../dist')))
-    // 因为是单页应用 所有请求都走/dist/index.html
+// 因为是单页应用 所有请求都走/dist/index.html
 app.get('/', function(req, res) {
     const html = fs.readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf-8')
     res.send(html)
 })
+//use使用路由中间件
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(api);
